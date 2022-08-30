f8c49e64e36e63b19fe918a78f97f1efaa961372
TaliaGtz <98000032+TaliaGtz@users.noreply.github.com>
2022-08-30 14:57:44 -0500

    Proyecto

---
+++ LogIn/LogIn.js
@@ -0,0 +1,43 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+var num = 2;
+navToggle.addEventListener("click", ()=>{
+    //navMenu.classList.toggle("nav-menu_visible");
+    
+    if(num == 1){
+        toggle(2);
+        num = 2;
+        return;
+    }
+    if(num == 2){
+        toggle(1);
+        num = 1;
+        return;
+    }
+});
+
+function toggle(num){
+    if(num == 1){
+        document.getElementById("navList").style = "display: block";
+        document.getElementById("regSpBtns").style = "height: 80px";
+        document.getElementById("iBars").classList.add("moveBars");
+        document.getElementById("iBars").classList.remove("resetBars");
+    }
+
+    if(num == 2){
+        document.getElementById("navList").style = "display: none";
+        document.getElementById("regSpBtns").style = "height: 80px";
+        document.getElementById("iBars").classList.add("resetBars");
+        document.getElementById("iBars").classList.remove("moveBars");
+    }
+}
+
+$(document).load($(window).bind("resize", checkPosition));
+
+function checkPosition()
+{
+    if($(window).width() > 843)
+    {
+        document.getElementById("navList").removeAttribute("style");
+    }
+}
