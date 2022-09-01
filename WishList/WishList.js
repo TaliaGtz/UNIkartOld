b191345ac28e3be6e008429e76e9464306bbe84b
TaliaGtz <98000032+TaliaGtz@users.noreply.github.com>
2022-08-31 23:28:32 -0500

    WishList

--- WishList/WishList.js
+++ WishList/WishList.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 var num = 2;
+var i = 0;
+var j = 0;
 navToggle.addEventListener("click", ()=>{
     //navMenu.classList.toggle("nav-menu_visible");
     
@@ -37,4 +39,45 @@ $(window).on("load",function checkPosition(){
     {
         document.getElementById("navList").removeAttribute("style");
     }
+});
+
+$("#publicar").click(function(){
+    i = i + 1;
+    var html = document.querySelector("#contenido");
+
+    panel = document.createElement('article');
+    panel.setAttribute('id', 'article' + i);
+    panel.setAttribute('class', 'article');
+    panel.setAttribute('style', 'display: block');
+    html.appendChild(panel);
+
+    var name = document.createElement('h2');
+    name.textContent = "Lista" + i;
+    panel.appendChild(name);
+
+    var msgRB = document.createElement('p');
+    msgRB.textContent = "categoría";
+    panel.appendChild(msgRB);
+
+    var msgRB = document.createElement('p');
+    msgRB.textContent = "privada/pública";
+    panel.appendChild(msgRB);
+    
+    msg = document.createElement('p');
+    msg.textContent = "Descripción";
+    panel.appendChild(msg);
+    /*
+    var datmsg = document.createElement('input');
+    datmsg.setAttribute('type', 'date');
+    datmsg.setAttribute('id', 'dateID' + i);
+    datmsg.setAttribute('class', 'datepkdP');
+    //console.log(date);
+    datmsg.setAttribute('value', date);
+    datmsg.setAttribute('disabled', 'true');
+    panel.appendChild(datmsg);*/
+});
+
+$("#eliminar").click(function(){
+    j = j + 1;
+    $("#article" + j).remove();       //Quita todo el código que tenga que ver con la lista
 });
