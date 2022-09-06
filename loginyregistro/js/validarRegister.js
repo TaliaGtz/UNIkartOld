2647189bd8fe4543aa244a1e15685aff9e7ca5e9
GerardoLeija01 <83870473+GerardoLeija01@users.noreply.github.com>
2022-09-05 22:53:01 -0500

    yes

--- loginyregistro/js/validarRegister.js
+++ loginyregistro/js/validarRegister.js
@@ -1,4 +1,23 @@
-function validar(params)
+function validarespcios()
 {
-   
+  names=document.getElementById("names").value;
+  lastnames=document.getElementById("lastnames").value;
+  birthday=document.getElementById("birthday").value;
+  email=document.getElementById("email").value;
+  profilepicture=document.getElementById("profilepicture").value;
+  username=document.getElementById("username").value;
+  password=document.getElementById("password").value;
+  confirmpassword=document.getElementById("confirmpassword").value;
+ tipo_de_usuario=document.getElementById("tipo_de_usuario").value;
+
+if(names.leght==0 || /^\s+$/.test(names)){
+alert("Inserte nombre");
+}
+
+
+
+
+
+
+
 }
