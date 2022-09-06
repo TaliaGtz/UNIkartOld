edaa7557872aba22e2dc7b515618821b9088912a
GerardoLeija01 <83870473+GerardoLeija01@users.noreply.github.com>
2022-09-05 23:26:36 -0500

    Update validarRegister.js

--- loginyregistro/js/validarRegister.js
+++ loginyregistro/js/validarRegister.js
@@ -1,14 +1,14 @@
-function validarespcios()
+function validarespacios()
 {
-  names=document.getElementById("names").value;
-  lastnames=document.getElementById("lastnames").value;
-  birthday=document.getElementById("birthday").value;
-  email=document.getElementById("email").value;
-  profilepicture=document.getElementById("profilepicture").value;
-  username=document.getElementById("username").value;
-  password=document.getElementById("password").value;
-  confirmpassword=document.getElementById("confirmpassword").value;
- tipo_de_usuario=document.getElementById("tipo_de_usuario").value;
+  names = document.getElementById("names").value;
+  lastnames = document.getElementById("lastnames").value;
+  birthday = document.getElementById("birthday").value;
+  email = document.getElementById("email").value;
+  profilepicture = document.getElementById("profilepicture").value;
+  username = document.getElementById("username").value;
+  password = document.getElementById("password").value;
+  confirmpassword = document.getElementById("confirmpassword").value;
+  tipo_de_usuario = document.getElementById("tipo_de_usuario").value;
 
 if(names.leght==0 || /^\s+$/.test(names)){
 alert("Inserte nombre");
