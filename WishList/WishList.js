110ea11ee98bd811f4ff897afc7e7edc80a41612
TaliaGtz <98000032+TaliaGtz@users.noreply.github.com>
2022-09-09 15:53:19 -0500

    Cart

--- WishList/WishList.js
+++ WishList/WishList.js
@@ -162,7 +162,7 @@ $("#cart").click(function(){
         '<p class="quantityNum">1</p>'+
         '<i class="fa-solid fa-square-plus quantity"></i>'+
     '</div>'+
-    '</form><div id="modButtons"><a id="mclose" href="#">',   
+    '</form><div><button id="buy"><a id="buyT" href="../SisPago/SisPago.html">Ir a pagar</a></button></div><div id="modButtons"><a id="mclose" href="#">',   
     'Cerrar');
 });
 
